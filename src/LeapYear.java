c091f309c7b0d9077ec7e6f6a3f5334ca65493b1
eduardo <eduardocardoso1039@gmail.com>
2023-09-18 20:01:21 -0300

    verify if year is leaped

---
+++ src/LeapYear.java
@@ -0,0 +1,20 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        isLeapYear(1924);
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    return year % 400 == 0;
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+}
