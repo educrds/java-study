906efb5da595925319840c42f5923d938fec217e
eduardo <eduardocardoso1039@gmail.com>
2023-09-21 21:53:57 -0300

    method to calculate years and days based on minutes

---
+++ src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,21 @@
+public class MinutesToYearsDaysCalculator {
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long hours = minutes / 60;
+
+            long day = hours / 24;
+            long remainingDays = day % 365;
+
+            long years = day / 365;
+
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+    }
+
+    public static void main(String[] args) {
+        printYearsAndDays(1051200);
+    }
+}
